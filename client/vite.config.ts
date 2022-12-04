7580875ae15861c0a7a5fd496948ff5cbf835d51
KostyaVid <constat@live.com>
2022-12-04 16:27:17 +0300

    add router

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        configure: (proxy, options) => {
+          // proxy will be an instance of 'http-proxy'
+        },
+      },
+      // Proxying websockets or socket.io
+      // '/socket.io': {
+      //   target: 'ws://localhost:5173',
+      //   ws: true,
+      // },
+    },
+  },
 });
